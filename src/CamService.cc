3e6d94e1f7f6032e7effc0de1f7e351a626b9851
Frank Hucek <me@frankhucek.com>
2025-07-09 13:30:32 -0600

    settings

--- src/CamService.cc
+++ src/CamService.cc
@@ -10,12 +10,12 @@ CamService::CamService(int* argc, char** argv[]) {
     #ifdef RPI_MODE
     recordingPipeline = make_unique<RecordingPipeline>(RECORDING_DIR, VIDEO_DURATION, argc, argv);
     recordingPipeline->setSource(std::make_unique<LibcameraPipelineSource>());
-    recordingPipeline->addSink(std::make_unique<TsFileSinkPipeline>(recordingPipeline.get(), false));
+    recordingPipeline->addSink(std::make_unique<TsFileSinkPipeline>(recordingPipeline.get(), true));
     recordingPipeline->addSink(std::make_unique<HlsPipelineSink>(recordingPipeline.get()));
     #else
     recordingPipeline = make_unique<RecordingPipeline>(RECORDING_DIR, VIDEO_DURATION, argc, argv);
     recordingPipeline->setSource(std::make_unique<V4l2PipelineSrc>());
-    recordingPipeline->addSink(std::make_unique<TsFileSinkPipeline>(recordingPipeline.get(), false));
+    recordingPipeline->addSink(std::make_unique<TsFileSinkPipeline>(recordingPipeline.get(), true));
     recordingPipeline->addSink(std::make_unique<HlsPipelineSink>(recordingPipeline.get()));
     #endif
 
